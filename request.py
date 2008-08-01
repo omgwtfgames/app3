f1f9b24c9b46c66891be17eab5ad10ec2ae8d8de
jgeewax <jgeewax@ccd4ad72-4b53-0410-b42e-f501c806baa6>
2008-08-01 14:10:28 +0000

    Forgot to add the app3 request.

---
+++ request.py
@@ -0,0 +1,34 @@
+from app3 import auth
+
+class App3Request(object):
+    """
+    A struct type object for storing the details of an App3 request
+    
+    This was built mostly 
+    """
+    format = "text"
+    app3_timestamp = None
+    app3_auth = None
+    
+    def __init__(self, request, secret_key=None):
+        """
+        """
+        self.path = request.path
+        self.method = request.method
+        self.secret_key = secret_key
+        self.params = dict([(str(key), request.get(key)) for key in request.params])
+        self.headers = request.headers
+        
+        if "format" in self.params:
+            self.format = self.params["format"]
+        
+        if "App3-Timestamp" in self.headers:
+            self.app3_timestamp = self.headers['App3-Timestamp']
+        
+        if "App3-Auth" in self.headers:
+            self.app3_auth = self.headers['App3-Auth']
+    
+    def authenticate(self):
+        return auth.is_authorized(self)
+    
+    
